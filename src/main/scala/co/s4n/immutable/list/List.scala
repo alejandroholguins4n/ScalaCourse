d4ed17bed723b0bde3665a12e401afe572dfea09
Alejandro Holguin <johanholguin@seven4n.com>
2021-02-25 12:59:45 -0500

    Basic list implementation

--- src/main/scala/co/s4n/immutable/list/List.scala
+++ src/main/scala/co/s4n/immutable/list/List.scala
@@ -1,3 +1,18 @@
 package co.s4n.immutable.list
 
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Const[+A](h: A, t: List[A]) extends List[A]
 
+object List {
+
+  def apply[A](as: A*) : List[A] = {
+    if (as.isEmpty) Nil
+    else Const(as.head, apply(as.tail : _*))
+  }
+
+  def length[A](lst : List[A]) : Int = lst match {
+    case Nil => 0
+    case Const(h, t) => 1 + length(t)
+  }
+}
